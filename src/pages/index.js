22acb7f30cd13a7f3e85dc2df90b4a4ea2ecb896
Yash Gediya <yash.gediya@bytestechnolab.in>
2022-07-12 12:40:30 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,12 @@ import * as styles from "../components/index.module.css"
 
 const IndexPage = () => {
   const clickHandle = () => {
-    window.gtag("Buttonevent", "Buttonclick", {
-      event_category: "category",
-      event_label: "label",
-      value: "value",
-    })
+    dataLayer.push({ event: "login" })
+    // window.gtag("Buttonevent", "Buttonclick", {
+    //   event_category: "category",
+    //   event_label: "label",
+    //   value: "value",
+    // })
   }
 
   return (
